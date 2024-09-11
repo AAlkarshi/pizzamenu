3640dbe9857fd0b095a2966e0292cb5f9c4c4cd7
Abdullrahman Al karshi <alkarshi.abdullrahman@gmail.com>
2024-09-11 20:47:23 +0200

    Application React qui simule un site web de pizzeria avec plusieurs composants : Header, Menu, Pizza et Footer. Le composant principal App structure l'application en affichant ces différents composants dans une div conteneur.
    Le composant Header affiche un titre avec un style personnalisé. Le composant Menu récupère une liste de pizzas depuis la variable pizzaData, et utilise une expression conditionnelle pour afficher soit une liste de pizzas disponibles, ou un message d'indisponibilité s'il n'y a pas de pizzas.
    
    Le composant Pizza prend en charge l'affichage de chaque pizza avec son nom, ses ingrédients, son prix, et une image correspondante. Si une pizza est épuisée (soldOut), la classe CSS applique un style différent pour griser l'élément et afficher le texte "STOCK ÉPUISÉ".
    
    Le composant Footer affiche un message d'ouverture basé sur l'heure actuelle et permet de passer une commande lorsque la pizzeria est ouverte, entre 11h et 23h. Le composant Horaire, utilisé dans le Footer, affiche les heures d'ouverture de la pizzeria.
    
    L'application récupère et rend la liste des pizzas en utilisant ReactDOM.createRoot pour l'affichage dynamique dans l'élément HTML avec l'ID "root".

--- src/index.js
+++ src/index.js
@@ -1,17 +1,169 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  
+  return ( 
+   <div className="container">
+     <Header/>
+     <Menu/>
+     <Footer/>
+   </div>
+ );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Header() {
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}> Fast React Pizza </h1> 
+    </header>
+  );
+}
+
+
+
+
+
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className='menu'>
+      <h2>Nos Menus</h2> 
+
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>Authentique cuisine Italienne. 6 créations, à vous de choisir.</p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} /> 
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>Désolé, aucun menu disponible pour le moment.</p>
+      )}
+    </main>
+  );
+}
+
+
+
+/* Composant PIZZA */
+function Pizza(props) {
+  console.log(props);
+  return (
+    //TERNAIRE si pizza Epuisé alors grisé
+  <li className={`pizza ${props.pizzaObj.soldOut ? "sold-out" : "" }`}>
+    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <div>
+      <h3> {props.pizzaObj.name}</h3> 
+      <p> {props.pizzaObj.ingredients} </p>
+      <span> {props.pizzaObj.soldOut ? "STOCK ÉPUISÉ" : props.pizzaObj.price}</span>
+    </div>
+  </li>
+ );
+}
+
+
+
+
+
+
+
+function Footer() {
+  const heure = new Date().getHours();
+  const OuvertureHeure = 11;
+  const FermetureHeure = 23;
+  const Ouvert = heure >= OuvertureHeure && heure <= FermetureHeure;
+
+  return ( 
+    <footer className="footer"> 
+      {OuvertureHeure && (
+        <div className="order"> 
+        <Horaire OuvertureHeure={OuvertureHeure} FermetureHeure={FermetureHeure} />
+            <button className="btn"> Commander</button>
+        </div>
+            )}  
+    </footer>
+)
+
+function Horaire({OuvertureHeure, FermetureHeure }) {
+  return (
+    <div>
+      <h2>Ouvert de {OuvertureHeure}:00 jusqu'à {FermetureHeure}:00 heures. </h2>
+    </div>
+  )
+}
+
+}
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(< App />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+
